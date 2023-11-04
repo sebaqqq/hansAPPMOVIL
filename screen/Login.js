ffc77ce6179dd5c335121113074067888268e523
fernet_en_mano <91861841+skantos@users.noreply.github.com>
2023-11-04 00:20:31 -0300

    login

--- screen/Login.js
+++ screen/Login.js
@@ -1,75 +1,82 @@
 import React from "react";
-import { Text, StyleSheet, View, TextInput, TouchableOpacity } from "react-native";
+import { Text, StyleSheet, View, TextInput, TouchableOpacity, Image } from "react-native";
 import "react-native-gesture-handler";
 
-function Login () {
+function Login() {
   return (
-    <View style={styles.padre}>
-      <Text>Hans Motors</Text>
-      <View style={styles.tarjeta}>
-        <Text>Iniciar Sesión</Text>
-        <TextInput 
-          placeholder="Correo electronico" 
-          style={styles.cajaTexto}
+    <View style={styles.container}>
+      <Image source={require('../images/car.png')} style={styles.logoImage} />
+
+      <Text style={styles.logo}>Hans Motors</Text>
+      <Text style={styles.heading}>Iniciar Sesión</Text>
+      <View style={styles.inputContainer}>
+        <TextInput
+          placeholder="Correo electrónico"
+          style={styles.input}
+          placeholderTextColor="#A0A0A0"
         />
         <TextInput
-          placeholder="Contraseña" 
-          style={styles.cajaTexto}
-          secureTextEntry={true}
+          placeholder="Contraseña"
+          style={styles.input}
+          secureTextEntry
+          placeholderTextColor="#A0A0A0"
         />
-        <View style={styles.padreBoton}>
-          <TouchableOpacity style={styles.boton}>
-            <Text style={styles.botonTexto}>Iniciar Sesión</Text>
-          </TouchableOpacity>
-        </View>
       </View>
+      <TouchableOpacity style={styles.loginButton}>
+        <Text style={styles.loginButtonText}>Iniciar Sesión</Text>
+      </TouchableOpacity>
     </View>
-  )
+  );
 }
 
 const styles = StyleSheet.create({
-  padre: {
+  container: {
     flex: 1,
     backgroundColor: "#fff",
-    alignItems: 'center',
+    alignItems: "center",
     justifyContent: "center",
+    paddingHorizontal: 20,
   },
-  tarjeta: {
-    margin: 20,
-    backgroundColor : "#fff",
-    borderRadius: 20,
-    width: '90%',
-    padding: 20,
-    shadowColor: "#000",
-    shadowOffset: {
-      width: 0,
-      height: 2,
-    },
-    shadowOpacity: 0.25,
-    shadowRadius: 4,
-    elevation: 5
+  logoImage: {
+    width: 150, // Ajusta el ancho según tus necesidades
+    height: 150, // Ajusta la altura según tus necesidades
+    marginBottom: 20,
   },
-  cajaTexto: {
-    paddingVertical:20,
-    backgroundColor: "#cccccc40",
-    borderRadius: 30,
-    marginVertical: 10,
-    paddingHorizontal:15,
+  logo: {
+    fontSize: 24,
+    fontWeight: "bold",
+    marginBottom: 20,
+    color: "#525FE1",
   },
-  padreBoton: {
-    alignItems: 'center'
+  heading: {
+    fontSize: 18,
+    fontWeight: "bold",
+    marginBottom: 20,
+    color: "#333",
   },
-  boton: {
+  inputContainer: {
+    width: "100%",
+    marginBottom: 20,
+  },
+  input: {
+    borderBottomWidth: 1,
+    borderBottomColor: "#525FE1",
+    paddingVertical: 10,
+    marginBottom: 10,
+    color: "#333",
+  },
+  loginButton: {
     backgroundColor: "#525FE1",
     borderRadius: 30,
-    paddingVertical: 20,
-    width: 150,
-    marginTop: 20
+    paddingVertical: 15,
+    width: "100%",
+    alignItems: "center",
   },
-  botonTexto: {
-    textAlign: 'center',
-    color: 'white'
-  }
+  loginButtonText: {
+    color: "white",
+    fontSize: 16,
+    fontWeight: "bold",
+  },
 });
 
 export default Login;
