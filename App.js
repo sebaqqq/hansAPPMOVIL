4ec0f73a2818be904366c6187dc65d436dc7dd28
Sebastián Quintana <101677885+sebaqqq@users.noreply.github.com>
2023-11-04 01:01:06 -0300

    menosCodigo

--- App.js
+++ App.js
@@ -1,6 +1,4 @@
-import { StyleSheet, Text, View } from 'react-native';
 import 'react-native-gesture-handler';
-
 import Navigation from './Navigation';
 
 export default function App() {
@@ -8,12 +6,3 @@ export default function App() {
     <Navigation />
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
